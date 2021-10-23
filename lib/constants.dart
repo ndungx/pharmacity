1f5b4c820cf9f726d27add516cc9c69aa526a20b
ndungx <dpron12345@gmail.com>
2021-10-24 02:34:07 +0700

    add and change some global constant

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0x0E62F9FF);
+const kPrimaryColor = Color(0xFF1562F9);
+const kPrimaryDarkColor = Color(0xFF001943);
+const kSecondaryColor = Color(0xFFCECECE);
+const kSuccessColor = Color(0xFF5CAB45);
 const kAnimationDuration = Duration(milliseconds: 200);
 
 // Form errors
-final RegExp phoneValidationRegExp =
-    RegExp('/(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})\b/');
+final RegExp phoneValidatorRegExp = RegExp(
+    '^(([03+[2-9]|05+[6|8|9]|07+[0|6|7|8|9]|08+[1-9]|09+[1-4|6-9]]){3})+[0-9]{7}\$');
 const String kPhoneNullError = "Vui lòng nhập số điện thoại của bạn";
-const String kInvalidInvalidError = "Vui lòng nhập đúng số điện thoại";
-const String kPassNullError = "Please Enter your password";
-const String kShortPassError = "Password is too short";
-const String kMatchPassError = "Passwords don't match";
+const String kInvalidPhoneError = "Vui lòng nhập đúng số điện thoại";
+const String kPhoneNumberNullError = "Vui lòng không bỏ trống số điện thoại";
+const String kShortPassError = "Mật khẩu quá ngắn";
+const String kMatchPassError = "Mật khẩu không đúng";
+const String kPassNullError = "Vui lòng không bỏ trống mật khẩu";
 const String kNamelNullError = "Please Enter your name";
-const String kPhoneNumberNullError = "Please Enter your phone number";
 const String kAddressNullError = "Please Enter your address";
