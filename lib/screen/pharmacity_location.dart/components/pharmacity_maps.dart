7d7c1a4548b1a9ab46ae4ea483fac2b12b1a1733
ndungx <dpron12345@gmail.com>
2021-11-02 15:38:48 +0700

    fix single child scroll view scrolling when move map

--- lib/screen/pharmacity_location.dart/components/pharmacity_maps.dart
+++ lib/screen/pharmacity_location.dart/components/pharmacity_maps.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -12,16 +14,27 @@ class PharmacityMaps extends StatelessWidget {
       child: GoogleMap(
         onMapCreated: (GoogleMapController controller) {},
         initialCameraPosition: const CameraPosition(
-          target: LatLng(10.8324174, 106.6827554),
+          target: LatLng(10.8105337, 106.6925944),
           zoom: 15,
         ),
         rotateGesturesEnabled: false,
         scrollGesturesEnabled: true,
         tiltGesturesEnabled: false,
+        gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>{
+          Factory<OneSequenceGestureRecognizer>(() => EagerGestureRecognizer()),
+        },
         markers: {
           const Marker(
             markerId: MarkerId('1'),
-            position: LatLng(10.8324174, 106.6827554),
+            position: LatLng(10.8105337, 106.6925944),
+            infoWindow: InfoWindow(
+              title: 'Pharmacity',
+              snippet: 'Pharmacity',
+            ),
+          ),
+          const Marker(
+            markerId: MarkerId('2'),
+            position: LatLng(10.8088265, 106.6904057),
             infoWindow: InfoWindow(
               title: 'Pharmacity',
               snippet: 'Pharmacity',
